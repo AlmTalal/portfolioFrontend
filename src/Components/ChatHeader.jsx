d9c60f9245d39ca92777fb2512a659eff4d519f2
Talal <alamtalal004@gmail.com>
2023-08-07 15:21:32 -0400

    handled the routing to the chatbot

--- src/Components/ChatHeader.jsx
+++ src/Components/ChatHeader.jsx
@@ -2,10 +2,16 @@ import { Avatar, Box, Typography } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
 import { tokens } from "../theme";
 import { useTheme } from "@emotion/react";
+import { useNavigate } from "react-router-dom";
 
-export default function ChatHeader({ hideChatBox }) {
+export default function ChatHeader() {
   const theme = useTheme();
   const colors = tokens(theme.pallete.mode);
+  const navigate = useNavigate();
+
+  const handleNavigate = (link) => {
+    navigate(link);
+  };
 
   return (
     <Box
@@ -30,7 +36,7 @@ export default function ChatHeader({ hideChatBox }) {
           Mr. Bolt
         </Typography>
       </Box>
-      <Box onClick={hideChatBox} sx={{ cursor: "pointer" }}>
+      <Box onClick={() => handleNavigate("/")} sx={{ cursor: "pointer" }}>
         <CloseIcon
           sx={{ width: 40, height: 40, color: colors.secondary[400] }}
         />
