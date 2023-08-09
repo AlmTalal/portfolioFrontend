2680c1b542713ab40e0b74375b8cf995b597d151
Talal <alamtalal004@gmail.com>
2023-08-09 04:39:03 -0400

    Trying to see why the api is not working

--- src/Screens/UptimeCheck.jsx
+++ src/Screens/UptimeCheck.jsx
@@ -3,22 +3,24 @@ import React, { useEffect, useState } from "react";
 import { socket } from "../Services/apiSocket";
 
 export default function UptimeCheck() {
-  const [messages, setMessages] = useState([]);
+  const [messages, setMessages] = useState();
   socket.on("webStatusIs", (newMessage) => {
     console.log(newMessage);
     //const msg = newMessage.message;
-    setMessages([...messages, "msg"]);
+    //setMessages([...messages, "msg"]);
+    setMessages(newMessage);
   });
 
   useEffect(() => {}, [messages]);
 
   return (
     <Box width={"100vw"} height={"100vh"}>
-      {messages.map((message, index) => (
+      {/* {messages.map((message, index) => (
         <Box width={"auto"} height={"auto"} color={"white"} key={index}>
           {message}
         </Box>
-      ))}
+      ))} */}
+      <Box>{messages}</Box>
     </Box>
   );
 }
